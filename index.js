16b72ae2dea028167e1fef88d4f86b2eafcd5e39
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-13 08:50:23 +0100

    Added chalk and diskspace

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+var chalk = require('chalk')
+var diskspace = require('diskspace')
+
+var data = {
+   diskspace : '?',
+   battery   : '?'
+}
+
 var bar = function() {
-    console.log('Batteriez 🔋')
+    console.log('%s | %s', chalk.white(data.diskspace), data.battery)
+    diskspace.check('/', function(err, total, free, status) {
+        data.diskspace = Math.round(free/1000000000) + ' GB'
+    })
 }
 
 setInterval(bar,2000); bar()
