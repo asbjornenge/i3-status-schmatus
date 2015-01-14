7fce6d63086b7926ea44f6f5fa50e60ce6fb9ec3
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-01-14 14:41:16 +0100

    Added some network info

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var chalk = require('chalk')
+var network = require('network')
 var diskspace = require('diskspace')
 var batteryInfo = require('battery-info')
 
+// missing..
+// load
+// time
+
 var data = {
+    network : '?',
     diskspace : '?',
     batteryStatus : '?',
     batteryPercent : '?'
@@ -11,7 +17,8 @@ var data = {
 var bar = function() {
     var disk = formatDiskInfo(data)
     var batt = formatBatteryInfo(data)
-    console.log('%s | %s', disk, batt) 
+    var netw = formatNetworkInfo(data)
+    console.log('%s | %s | %s', disk, netw, batt) 
     diskspace.check('/', function(err, total, free, status) {
         if (err) return
         data.diskspace = Math.round(free/1000000000) + ' GB'
@@ -20,6 +27,10 @@ var bar = function() {
         if (err) return
         data.batteryStatus = info.status
         data.batteryPercent = ((100 / info.energy_full) * info.energy_now).toFixed(2)
+    })
+    network.get_interfaces_list(function(err, list) {
+        if (err) return
+        data.network = list
     })
 }
 
@@ -31,6 +42,17 @@ function formatBatteryInfo(data) {
     var battcolor = data.batteryStatus == 'Discharging' ? chalk.cyan : chalk.green
     if (data.batteryStatus == 'Discharging' && parseFloat(data.batteryPercent) < 10) battcolor = chalk.red
     return battcolor('🔋  '+data.batteryPercent+'%')
+}
+
+function formatNetworkInfo(data) {
+    if (data.network == '?') return
+    if (data.network instanceof Array && data.network.length == 0) return 'No network'
+    var infostr = ''
+    data.network.forEach(function(network) {
+        infostr += ' '
+        infostr += chalk.green(network.ip_address)
+    })
+    return infostr
 }
 
 setInterval(bar,2000);bar();
